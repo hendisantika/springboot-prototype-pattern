5d94cf7cd9faa030187c861e49ad19c22c2f1dac
Hendi Santika <hendisantika@gmail.com>
2020-01-04 05:28:40 +0700

    #5 - Add Role Enum

---
+++ src/main/java/com/hendisantika/springbootprototypepattern/model/Role.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootprototypepattern.model;
+
+import org.springframework.context.annotation.Description;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-prototype-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/01/20
+ * Time: 05.28
+ */
+@Description(value = "Potential roles.")
+public enum Role {
+
+    PROJECT_MANAGER, DEVELOPER
+
+}
