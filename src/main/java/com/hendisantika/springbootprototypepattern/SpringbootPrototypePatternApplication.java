3c9c05ded3868d2be32560a4c0acaa02e3e16101
Hendi Santika <hendisantika@gmail.com>
2020-01-04 05:31:01 +0700

    #8 - Add Init Data in Main Class

--- src/main/java/com/hendisantika/springbootprototypepattern/SpringbootPrototypePatternApplication.java
+++ src/main/java/com/hendisantika/springbootprototypepattern/SpringbootPrototypePatternApplication.java
@@ -1,13 +1,37 @@
 package com.hendisantika.springbootprototypepattern;
 
+import com.hendisantika.springbootprototypepattern.model.Role;
+import com.hendisantika.springbootprototypepattern.prototype.Developer;
+import com.hendisantika.springbootprototypepattern.prototype.ProjectManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootPrototypePatternApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootPrototypePatternApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootPrototypePatternApplication.class, args);
+
+		// create original instance of developer...
+		Developer hDeveloper = new Developer("Heril Muratovic", Role.DEVELOPER);
+		LOGGER.info("hDeveloper - original instance: {}", hDeveloper);
+
+		// create prototype of developer...
+		Developer jDeveloper = (Developer) hDeveloper.cloneObject();
+		jDeveloper.setFullName("Jane Doe");
+
+		// logging the output...
+		LOGGER.info("hDeveloper - again original instance: {}", hDeveloper);
+		LOGGER.info("jDeveloper - prototype instance: {}", jDeveloper);
+
+		ProjectManager jProjectManager = new ProjectManager("John Doe", Role.PROJECT_MANAGER);
+		ProjectManager aProjectManager = (ProjectManager) jProjectManager.cloneObject();
+
+		LOGGER.info("Project Manager - Original instance: {}", jProjectManager);
+		LOGGER.info("Project Manager - Prototype instance: {}", aProjectManager);
 	}
 
 }
